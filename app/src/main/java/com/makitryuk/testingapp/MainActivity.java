b690754b423f3c80148c2dfe2db40e153035bc88
irXak <97681718+irXak@users.noreply.github.com>
2022-02-08 23:27:16 +0900

    add to activity_signin.xml

--- app/src/main/java/com/makitryuk/testingapp/MainActivity.java
+++ app/src/main/java/com/makitryuk/testingapp/MainActivity.java
@@ -3,12 +3,26 @@ package com.makitryuk.testingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnSignIn, btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
