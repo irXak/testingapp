b18b2da55733a90b783645f77772d1fa85f332f0
Diomnas <makitryuk.d@gmail.com>
2022-03-31 15:20:01 +0900

    Добавлен рекламный блок который к сожалению из-за блокировок не работает поэтому закоментирован. Добавлен аудио файл для воспроизведения фоном в отдельном потоке при запуске приложения, но к сожалению пока музыка при запуске не играет добавлен класс Music.java

--- app/src/main/java/com/makitryuk/testingapp/MainActivity.java
+++ app/src/main/java/com/makitryuk/testingapp/MainActivity.java
@@ -3,11 +3,22 @@ package com.makitryuk.testingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.makitryuk.testingapp.Models.Music;
+
 public class MainActivity extends AppCompatActivity {
+
+    //private static final String TAG = "MainActivity";    //рекламный тэг
 
     private Button btnSignIn, btnSignUp;
 
@@ -16,12 +27,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        new Music();
+
         String activeUser = Signin.getDefaults("phone", MainActivity.this); // проверяем авторизован ли пользователь и если он авторизован то показывать страничку фуд_пейдж
 
         if (activeUser != null && activeUser.equals("")) {  // создадим условие: если эктив юзер не будет равен пустой строке, то перебрасываем пользователя на другую страницу
             Intent intent = new Intent(MainActivity.this, FoodPage.class);
             startActivity(intent);
         }
+//--------------------вызов рекламного блока----------------------
+
+//        mAdView = findViewById(R.id.adView);
+//        AdRequest adRequest = new AdRequest.Builder().build();
+//        mAdView.loadAd(adRequest);  //Пока гугл в блоке не активно
+//---------------------END----------------------------------------------------
 
         btnSignIn = findViewById(R.id.btnSignIn);
         btnSignUp = findViewById(R.id.btnSignUp);
